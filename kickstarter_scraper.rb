1e229753962575673367913acce1b09fd4c58c6b
Brian Pompey <brianthescholar@gmail.com>
2019-10-11 02:49:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,11 +2,6 @@ require 'nokogiri'
 require 'pry'
 
 
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
