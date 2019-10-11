c1f7c32708100a25c59168f4376c32cb51756795
Brian Pompey <brianthescholar@gmail.com>
2019-10-11 02:49:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,7 @@
 require 'nokogiri'
 require 'pry'
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
- image link: project.css("div.project-thumbnail a img").attribute("src").value
+
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
@@ -19,6 +17,9 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+      :description => project.css("p.bbcard_blurb").text
+      :location => project.css("ul.project-meta span.location-name").text
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
 
